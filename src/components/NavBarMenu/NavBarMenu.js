cec91f07c6c5e7e16b7587bd71913d4444f8cdae
Celinesco <celina.alsina@gmail.com>
2022-02-20 19:41:33 +0100

    responsive cards y paginas inicio mq 991

--- src/components/NavBarMenu/NavBarMenu.js
+++ src/components/NavBarMenu/NavBarMenu.js
@@ -7,7 +7,7 @@ const NavBarMenu = ({handleClickLinkMenu, setNavMenu}) => {
         <nav className='container__navbar-menu'>
             <button onClick={() => setNavMenu(false)} className='button__close-nav-menu'><BiX /></button>
              <ul className='ul__navmenu'>
-                    <li><Link to="/">HOME</Link></li>
+                    <li><Link onClick={()=>setNavMenu(false)}to="/">HOME</Link></li>
                     <li><Link onClick={handleClickLinkMenu} to="/characters">CHARACTERS</Link></li>
                     <li><Link onClick={handleClickLinkMenu} to="/episodes">EPISODES</Link></li>
                     <li><Link onClick={handleClickLinkMenu} to="/locations">LOCATIONS</Link></li>
