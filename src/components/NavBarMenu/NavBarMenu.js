e320f0a0a1192d45015080bc7e1a11e7ac2e2e67
Celinesco <celina.alsina@gmail.com>
2022-02-24 11:26:00 +0100

    corrijo rutas del menu hamburguesa

--- src/components/NavBarMenu/NavBarMenu.js
+++ src/components/NavBarMenu/NavBarMenu.js
@@ -8,9 +8,9 @@ const NavBarMenu = ({handleClickLinkMenu, setNavMenu}) => {
             <button onClick={() => setNavMenu(false)} className='button__close-nav-menu'><BiX /></button>
              <ul className='ul__navmenu'>
                     <li><Link onClick={()=>setNavMenu(false)}to="/">HOME</Link></li>
-                    <li><Link onClick={handleClickLinkMenu} to="/characters">CHARACTERS</Link></li>
-                    <li><Link onClick={handleClickLinkMenu} to="/episodes">EPISODES</Link></li>
-                    <li><Link onClick={handleClickLinkMenu} to="/locations">LOCATIONS</Link></li>
+                    <li><Link onClick={handleClickLinkMenu} to="/RickAndMortyPage/characters">CHARACTERS</Link></li>
+                    <li><Link onClick={handleClickLinkMenu} to="/RickAndMortyPage/episodes">EPISODES</Link></li>
+                    <li><Link onClick={handleClickLinkMenu} to="/RickAndMortyPage/locations">LOCATIONS</Link></li>
             </ul>
         </nav>
     )
